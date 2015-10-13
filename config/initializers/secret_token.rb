140b6a134747d3c2213354e29c0157f5a6b32320
Jaime Moreno <j7isaac@gmail.com>
2015-10-13 22:53:48 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Vizil::Application.config.secret_key_base = 'e6e830295eb949fec4346bb2f742eac4ddfd4f096fbbe2e281daa6db512c14fb2decef32679e879a8dcbb645b4b95b3bedc34a2d38a9e99419e77401d446766c'
