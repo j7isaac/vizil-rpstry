55b67dba788d89b4e7c444a82335a38513b4f74d
Jaime Moreno <j7isaac@gmail.com>
2015-10-26 00:05:44 +0000

    Moved the bootstrap form to a modal

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Vizil::Application.routes.draw do
   root 'quotes#index'
-
-  resources :quotes 
+  resources :quotes
+  get 'about', :to => 'quotes#about' 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
